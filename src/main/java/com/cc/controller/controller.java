5e8106175cbb0dbfb9fa0354b4ca7562b2ed19ed
FerrariCalifornia <598855214@qq.com>
2017-03-01 20:19:48 +0800

    build the framework

---
+++ src/main/java/com/cc/controller/controller.java
@@ -0,0 +1,7 @@
+package com.cc.controller;
+
+/**
+ * Created by cc on 2017/3/1.
+ */
+public class controller {
+}
---
+++ src/main/java/com/cc/pojo/UserRole.java
@@ -0,0 +1,53 @@
+package com.cc.pojo;
+
+public class UserRole {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private String registrationCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    public void setRegistrationCode(String registrationCode) {
+        this.registrationCode = registrationCode;
+    }
+}
